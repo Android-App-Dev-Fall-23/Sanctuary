b87b3ccf7dd8b1f8cb002a3a0c177854515d1302
Jawad <jawadandall@gmail.com>
2023-11-09 16:07:51 -0500

    Pet Object Created

---
+++ app/src/main/java/com/example/sanctuary/Pet.kt
@@ -0,0 +1,16 @@
+package com.example.sanctuary
+
+class Pet (
+    val name: String,
+    val species: String,
+    val breed: String,
+    val color: String,
+    val age: Int,
+    val ownerContact: String,
+    val lostLocation: String,
+    val otherDetails: String
+)
+{
+        var found: Boolean = false
+
+    }
